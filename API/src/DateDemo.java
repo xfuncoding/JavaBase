b5b445b377bb49f7dd4a1680e0f6b1c3cc810144
walf <chenxiaowei@quiknos.com>
2020-01-08 16:06:45 +0800

    ddd

---
+++ API/src/DateDemo.java
@@ -0,0 +1,5 @@
+public class DateDemo {
+    public static void main(String[] args) {
+        System.out.println("h");
+    }
+}
---
+++ 常用API/src/DemoDate.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class DemoDate {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+}
